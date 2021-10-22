b22e77781592c16cddb755aef803e212e68db6af
jca26c <78522594+jca26c@users.noreply.github.com>
2021-10-22 11:48:39 -0500

    Create views_irish.py

---
+++ main/views_irish.py
@@ -0,0 +1,28 @@
+## Import necessary packages
+from django.http.response import JsonResponse
+import requests 
+import json
+from django.shortcuts import render
+from urllib.parse import parse_qs, urlparse
+from hunspell import Hunspell
+
+## define hunspell variables
+h = Hunspell()
+
+## render views function
+def index(request):
+    return render(request,"main/index_irish.html")
+
+## hunspell check function
+def spell_check(request):
+    word_text=request.GET.get('word')
+    print(word_text)## for testing in console
+    word_string= json.dumps(word_text)
+    print(word_string)## for testing in console
+    replaced_string = word_string.replace('"', "")
+    print(replaced_string)## for testing in console
+    word_list= h.suggest(replaced_string)
+    print(word_list)## for testing in console
+    data ={'output': word_list}
+    print(data)## for testing in console
+    return JsonResponse(data)
