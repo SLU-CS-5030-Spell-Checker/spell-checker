c6eac6b37317c043aa929acff42da6d42aba198e
megannigg <89613483+megannigg@users.noreply.github.com>
2021-11-16 15:38:29 -0600

    adding files to create frequency files and models
    This is slightly different than the original files i pushed yesterday. the langmodel will now return 0.1 if no occurrences were found, this is to make sure than it will not cancel out the error model.

---
+++ combinedmodels.py
@@ -0,0 +1,82 @@
+import pickle
+
+
+#returns frequency of word in corpus
+def errormodel(word):
+    f = open('singlewordfreq','rb')
+    allwords = pickle.load(f)
+    f.close()
+    for i in allwords:
+        if i[1] == word:
+            return i[0]
+    return 0
+
+#returns frequency of word given previous word (Ngram)
+def langmodel(words, word):
+    try:
+        if len(words.split())==2:  #loads in different file depending on n
+            f = open('multiwordfreq','rb')
+        else:
+            f = open('multiwordfreq3','rb')
+        new_dict = pickle.load(f)
+        f.close()
+        w = tuple(words.split())
+        options = new_dict.get(w)
+        freq = options.get(word)
+        if freq == None:
+            freq=0.1
+        return freq
+    except:
+        return 0.1
+
+#determines final probability by combining errormodel and langmodel
+def totalprob(words, word):
+    return (errormodel(word)*langmodel(words,word))
+
+
+#creates list of eligiable words one edit dist apart
+def oneEditDist(word):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in alpha]
+    inserts    = [L + c + R               for L, R in splits for c in alpha]
+    return list(deletes + transposes + replaces + inserts)
+
+#creates list of eligiable words two edit dist apart
+def twoEditDist(word):
+    return list(e2 for e1 in oneEditDist(word) for e2 in oneEditDist(e1))
+
+#returns the list of one edit dist (can be both but it is slow)
+def possible(word):
+    return oneEditDist(word)
+
+#filters a list of words compared to the corpus
+def wordFilter(words):
+    f = open('singlewordfreq','rb')
+    allwords = pickle.load(f)
+    f.close()
+    real = []
+    for w in words:
+        for i in allwords:
+            if i[1] == w:
+                real.append(w)
+    return real
+
+#sorts list of words by total probability
+def correction(words, word): 
+    true = wordFilter(possible(word))
+    pc = []
+    for w in true:
+        pc.append(totalprob(words,w))
+    d = dict(zip(true,pc))
+    marklist=sorted((value, key) for (key,value) in d.items())
+    sortdict=dict([(k,v) for v,k in marklist])
+    bestwords = (list(sortdict.keys()))
+    bestwords.reverse()
+    return bestwords
+
+
+#example of how to call above functions
+print(correction('the blood','wih'))
---
+++ frequencycounter.py
@@ -0,0 +1,59 @@
+import random, re, codecs, sys, pickle
+
+#this reads in a txt file and save a list of tuples (frequency, word) in a pickle file
+def singleWordFrequency(txt, filename):
+    string = open(txt).read().lower()
+    freqdict = dict()
+    pattern = re.compile(r"([A-Za-z]+)") #removes unneeded punctuation
+    for word in re.findall(pattern, string):
+        if word in freqdict:	#if word is already accounted for increase count
+            freqdict[word] += 1
+        else:
+            freqdict[word] = 1 #if not create a new word with a count of 1
+    words = []
+    outfile = open(filename, 'wb')
+    for word in sorted(freqdict, key=freqdict.get, reverse=True):
+        words.append((freqdict[word],word))
+    pickle.dump(words, outfile)  #saved using pickle for ease of indexing
+    outfile.close()
+
+#this reads in a txt file and save a dictionary of words and frequency {(word,word): {word:freq}} in a pickle file       
+def multiWordFrequency(txt, n, filename):
+    answer = ()
+    pattern = re.compile(r"([A-Za-z]+)")  #removes unneeded punctuation
+    with codecs.open(txt, 'r', 'utf-8') as file:
+        for line in file:
+            line = line.lower()
+            answer = answer + tuple(re.findall(pattern, line))
+    model = dict()
+    for i in range(len(answer)-n+1):
+        key = answer[i:i+n-1]
+        val = answer[i+n-1]
+        if key in model:
+            if val in model[key]:
+                model[key][val] += 1 #if key is already accounted for increase count
+            else:
+                model[key][val] = 1  #if not create a new key
+        else:
+            model[key] = dict()
+            model[key][val] = 1
+    outfile = open(filename,'wb')
+    pickle.dump(model,outfile)  #saved using pickle for ease of indexing
+    outfile.close()
+
+
+
+#driver code to create files
+textfile = 'bible.txt'
+
+singleWordFrequency(textfile, 'singlewordfreq_irish')
+
+multiWordFrequency(textfile, 3, 'multiwordfreq_n3_irish')
+
+multiWordFrequency(textfile, 2, 'multiwordfreq_n2_irish')
+
+
+
+
+    
+    
