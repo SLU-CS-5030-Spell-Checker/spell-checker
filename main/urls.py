ac02525176e04ce36423b3e10d4573c1cdff8bc6
jca26c <78522594+jca26c@users.noreply.github.com>
2021-12-08 00:52:55 -0600

    Update urls.py
    Minor updates.  Adding new URL paths.

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 		path('', views.index),
 		path('', views_irish.index),
 		path('spell_check_irish', views_irish.spell_check),
-		path('spell_check', views.spell_check)
+		path('spell_check2_irish', views_irish.spell_check2),
+		path('spell_check', views.spell_check),
+		path('spell_check2', views.spell_check2)
 ]
