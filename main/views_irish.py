07657ec59639363a584b979399a58ff314a649dd
jca26c <78522594+jca26c@users.noreply.github.com>
2021-12-08 01:01:40 -0600

    Update views_irish.py
    The same functionality was added to the irish_views page as the english views page, two new functions to handle the back end of the context sensitive right click event on the front end.  get_context_words grabs the preceding two words of the word in the right click event.  clean_context_words edits the list of preceding words and deletes it if it is a misspelling.  Edits to the spell_check function were also made to read in the preceding two words into the corrections function.

--- main/views_irish.py
+++ main/views_irish.py
@@ -1,105 +1,111 @@
 import pickle
 from django.http.response import JsonResponse
-import requests 
 import json
 from django.shortcuts import render
 from urllib.parse import parse_qs, urlparse
 import re
+import re
+from collections import Counter
+from heapq import nlargest
+import pickle
 
-#returns frequency of word in corpus
-def errormodel(word):
-    f = open(r'C:\Users\jca26\OneDrive\Documents\csci_5030\singlewordfreq_irish.txt','rb')
-    allwords = pickle.load(f)
+
+def words(text): return re.findall(r'\w+', text.lower())
+WORDS = Counter(words(open(r'C:\Users\jca26\OneDrive\Documents\csci_5030\irish\corpus.txt',encoding='utf-8').read()))
+def totalProb(words, word): 
+    #lang model
+    if len(words.split())==2:
+        f = open(r'C:\Users\jca26\OneDrive\Documents\csci_5030\multiwordfreq_irish_n2.txt','rb')
+    else:
+        f = open(r'C:\Users\jca26\OneDrive\Documents\csci_5030\multiwordfreq_irish_n3.txt','rb')
+    new_dict = pickle.load(f)
     f.close()
-    for i in allwords:
-        if i[1] == word:
-            return i[0]
-    return 0
-
-#returns frequency of word given previous word (Ngram)
-def langmodel(words, word):
-    try:
-        if len(words.split())==2:  #loads in different file depending on n
-            f = open(r'C:\Users\jca26\OneDrive\Documents\csci_5030\multiwordfreq_irish_n2.txt','rb')
-        else:
-            f = open(r'C:\Users\jca26\OneDrive\Documents\csci_5030\multiwordfreq_irish_n3.txt','rb')
-        new_dict = pickle.load(f)
-        f.close()
-        w = tuple(words.split())
-        options = new_dict.get(w)
+    w = tuple(words.split())
+    options = new_dict.get(w)
+    if options == None:
+        freq = 0
+    else:
         freq = options.get(word)
         if freq == None:
-            freq=0.1
-        return freq
-    except:
-        return 0.1
+            freq = 0
+    N=sum(WORDS.values())
+    return (WORDS[word] / N + freq)
 
-#determines final probability by combining errormodel and langmodel
-def totalprob(words, word):
-    return (errormodel(word)*langmodel(words,word))
-
-
-#creates list of eligiable words one edit dist apart
 def oneEditDist(word):
-    alpha = 'abcdefghilmnoprstuáéíóú'
+    letters    = 'abcdefghilmnoprstuáéíóú'
     splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
     deletes    = [L + R[1:]               for L, R in splits if R]
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in alpha]
-    inserts    = [L + c + R               for L, R in splits for c in alpha]
-    return list(deletes + transposes + replaces + inserts)
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+def twoEditDist(word): 
+    return (e2 for e1 in oneEditDist(word) for e2 in oneEditDist(e1))
+def wordFilter(words): 
+    return set(w for w in words if w in WORDS)
 
-#creates list of eligiable words two edit dist apart
-def twoEditDist(word):
-    return list(e2 for e1 in oneEditDist(word) for e2 in oneEditDist(e1))
+def possible(word): 
+    ans = wordFilter(oneEditDist(word))
+    ans.update(wordFilter(twoEditDist(word)))
+    return ans
 
-#returns the list of one edit dist (can be both but it is slow)
-def possible(word):
-    return oneEditDist(word)
-
-#filters a list of words compared to the corpus
-def wordFilter(words):
-    f = open(r'C:\Users\jca26\OneDrive\Documents\csci_5030\singlewordfreq_irish.txt','rb')
-    allwords = pickle.load(f)
-    f.close()
-    real = []
-    for w in words:
-        for i in allwords:
-            if i[1] == w:
-                real.append(w)
-    return real
-
-#sorts list of words by total probability
-def correction(words):
-    word = words.split()[-1]
-    words = ' '.join(words.split()[:-1])
-    print(word)
-    print(words)
-    true = wordFilter(possible(word))
-    pc = []
-    for w in true:
-        pc.append(totalprob(words,w))
-    d = dict(zip(true,pc))
-    marklist=sorted((value, key) for (key,value) in d.items())
-    sortdict=dict([(k,v) for v,k in marklist])
-    bestwords = (list(sortdict.keys()))
-    bestwords.reverse()
-    return bestwords
-
-## render views function
+def correction(words, word): 
+    d = {}
+    for w in possible(word):
+        if word[0].isupper():
+            d[w.capitalize()] = totalProb(words, w)
+        else:
+            d[w] = totalProb(words, w)
+    return nlargest(3,d, key=d.get)
+    
 def index(request):
     return render(request,"main/index_irish.html")
 
-## Spell check function
+## spell check function
 def spell_check(request):
     word_text=request.GET.get('word')
-    print(word_text)## for testing in console
+    words=request.GET.get('words')
     word_string= json.dumps(word_text)
-    print(word_string)## for testing in console
+    words_string= json.dumps(words)
     replaced_string = word_string.replace('"', "")
-    print(replaced_string)## for testing in console
-    word_list= correction(replaced_string)
-    print(word_list)## for testing in console
+    ##replaced_string='prayd'
+    ##words_string='the camel'
+    word_list= correction(words_string,replaced_string)
+    print(word_list)
     data ={'output': word_list}
-    print(data)## for testing in console
     return JsonResponse(data)
+
+def spell_check2(request):
+    word_input= request.GET.get('word_list')
+    word_list= json.loads(word_input)
+    misspelled_words=[]
+    for i in word_list:
+        suggestion= wordFilter([i])
+        if not suggestion:
+            misspelled_words.append(i)
+    data={'output':json.dumps(misspelled_words)}
+    return JsonResponse(data)
+
+
+def get_context_words(word,words):
+    num_context_words= 3
+    word_index= words.index(word)
+    start_context_index= word_index-num_context_words
+    if start_context_index<=0:
+        start_context_index=0
+    context_words=[]
+    for x in range(start_context_index,word_index):
+        context_words.append(words[x])
+    clean_words= clean_context_words(context_words)
+    return ' '.join(clean_words)
+
+def clean_context_words(dirty_words):
+    for i in range(len(dirty_words)-1,-1,-1):
+        suggestion= wordFilter([dirty_words[i]])
+        if suggestion:
+            continue
+        else:
+            for j in range(i,-1,-1):
+               del dirty_words[j]
+            break
+    return dirty_words
